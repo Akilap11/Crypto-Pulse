036010051df55e8671acd66860c6bc5a4338502c
Akila Tharinda <watlankathilaka@students.nsbm.ac.lk>
2025-04-30 00:49:14 +0530

    added navbar & updated page styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src = {logo} alt="Spotify Logo" className='logo' />
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">Features</a></li>
+            <li><a href="#services">Pricing</a></li>
+            <li><a href="#contact">Blog</a></li>
+        </ul>
+        <div className="nav-right">
+            <select>
+                <option value="usd">USD</option>
+                <option value="lkr">LKR</option>
+                <option value="inr">INR</option>
+            </select>
+            <button className='btn'>Sign Up<img src={arrow_icon} alt=''></img></button>
+            <button className='btn'>Login</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
